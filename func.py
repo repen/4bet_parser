e32cdc8eb3956218991619325dcc409a48e67bf9
repen <9keepa@gmail.com>
2020-10-01 19:03:34 +0500

    tracking bet changed on N > 2.5

--- func.py
+++ func.py
@@ -7,7 +7,7 @@ def marathon(html):
     soup = BeautifulSoup(html, "html.parser")
     result = soup.find('div', text = re.compile('Тотал сетов'), attrs = {'class' : 'name-field'})
     if result:
-        koef = result.parent.parent.parent.parent.parent.select_one('span[data-selection-key*="Total_Sets.Over_2.5"]')
+        koef = result.parent.parent.parent.parent.parent.select_one('span[data-selection-key*="Total_Sets.Under_2.5"]')
         if koef:
             value = koef.text.strip()
             
@@ -21,7 +21,7 @@ def betcity(html):
         koef = re.findall( r"\d\.\d{1,4}", str(result.parent.parent))
         if koef:
             if len(koef) > 2: 
-                value = koef[2]
+                value = koef[1]
 
     return str(value).strip()
 
@@ -30,7 +30,7 @@ def ligastavok(html):
     soup = BeautifulSoup(html, "html.parser")
     result = soup.find('span', text = re.compile('Тотал сетов'))
     if result:
-        res = re.findall(r"\d\,\d{1,4}", str(result.parent.select_one('div[title*="бол"]')))
+        res = re.findall(r"\d\,\d{1,4}", str(result.parent.select_one('div[title*="мен"]')))
         if res:
             value = res[0]
 
@@ -43,14 +43,14 @@ def xbet(html):
     result = soup.find(text = re.compile('Тотал по сетам точный'))
     if result:
         # import pdb;pdb.set_trace()
-        res = re.findall(r"\d\.\d{1,4}", str(result.parent.parent.find(text=re.compile('^3\n')).parent.parent ) )
+        res = re.findall(r"\d\.\d{1,4}", str(result.parent.parent.find(text=re.compile('^2\n')).parent.parent ) )
         if res:
             value = res[0]
 
     return str(value).strip()
 
 func_dict = {
-    "xbet" : xbet,
+    "1xstavka" : xbet,
     "betcity":betcity,
     "marathon":marathon,
     "ligastavok":ligastavok
